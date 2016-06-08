49b8b53c3c8f18c19981d9938fbc451e4b0a30df
Prachi <prachi121096@gmail.com>
2016-06-09 01:52:56 +0530

    unnecessary files removed

--- inc/connection.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$host='localhost'; 
-	$username='root';  
-	$password=''; 
-	$db_name='profanity_check'; 
-	if(!@$connection=mysqli_connect($host, $username, $password,$db_name))
-	 die("Cannot connect"); 
-?>
