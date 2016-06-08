157d0d54f155f296a36abd73d9374c98e638582a
Prachi <prachi121096@gmail.com>
2016-06-08 23:31:41 +0530

    UI changes

---
+++ inc/layout/navbar.inc.php
@@ -0,0 +1,54 @@
+<nav class="navbar navbar-fixed-top navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">Profanity Checker</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-left">
+                <li><a data-toggle="modal" data-target="#myModal">Instructions</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Profanity Checker</h4>
+            </div>
+            <div class="modal-body text-justify">
+                <strong>Profanity Checker</strong> is a PHP based web-app to check large text blocks for NSFW content<br>
+                <hr>
+                How to use:
+                <ul>
+                    <li>
+                        <strong>Standard Mode</strong><br>
+                        <pre>Examines the piece of text against the <a href="https://en.wikipedia.org/wiki/WDYL_(search_engine)">What Do You Love</a>Project by Google that has now been depriciated</pre>
+                    </li>
+                    <li>
+                        <strong>Custom Check</strong><br>
+                        <pre>Allows you to create a custom database of words that you may deem inappropriate. This database keeps updating each time you enter a list of word. Entries are retained even after search.</pre>
+                    </li>
+                    <li>
+                        <strong>Selective Removal</strong><br>
+                        <pre>Allows you to search for certain custom words but doesn't save them in database. If you want to avoid entering the same words again and again, use the create custom check mode.</pre>
+                    </li>
+                </ul>  
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+<br><br><br>
--- index.php
+++ index.php
@@ -1,45 +1,148 @@
+<?php
 
-<html>
-	<div class="container">
-	    <table width="500" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#cccccc">
-	        <tr>
-	            <form method="POST" action="check.php" >
-	                <td>
-	                    <table width="100%" border="0"  cellspacing="1" bgcolor="#ffffff">
-	                        <tr>
-	                            <td colspan="3"><strong><center>Enter text input to check</cemter></strong></td>
-	                        </tr>
-	                        <tr>
-	                            <td width="78">Text input</td>
-	                            <td width="6">:</td>
-	                            <td width="500" ><textarea name="sentence" cols="50" rows="10"></textarea></td>
-	                        </tr>
-	                        <tr></tr>
-	                        <tr>
-	                        	<td colspan="6"><label><input type="checkbox" name="checktype" value="standard">Standard check</label><br></td>
-	                        </tr>
- 	                        <tr>
-	                        	<td colspan="6"><label><input type="checkbox" name="checktype" value="createcustom">Create Custom check</label><br></td>
-	                        </tr>
-	                        <tr>
-	                        	<td colspan="6"><label><input type="checkbox" name="checktype" value="rmselect">Selective removal</label><br></td>
-	                        </tr>
-	                        <tr>
-	                        	<td width="78">Custom list</td>
-	                            <td width="6">:</td>
-	                            <td colspan="6"><textarea name="list" cols="50" rows="2"></textarea></td>
-	                        </tr>			
-	                        <tr>
-	                            <td>&nbsp;</td>
-	                            <td>&nbsp;</td>
-	                            <td><input type="submit" name="Submit" value="GO"></td>
-	                         </tr>
-	                    </table>
-	                </td>
-	            </form>
-	        </tr>
-	    </table>
-	</div>
-</html>
+	require_once 'inc/connection.inc.php';
+	require_once 'inc/function.inc.php';
+	include 'inc/layout/header.inc.php';
+    include 'inc/layout/navbar.inc.php';
+    error_reporting(E_ERROR);
+	
+	if(!isset($_POST['sentence']) && !isset($_POST['checktype']))
+		header("Location : index.php");
+	else 
+	{
+		$sentence= $_POST['sentence'];
+		$check = $_POST['checktype'];
+
+		if($check=="createcustom") //Just entering into database
+		{
+			$list= $_POST['list'];
+			$list = preg_replace("/[^a-zA-Z 0-9]+/", " ", $list);
+			$list_split =explode(" ", $list);
+
+			foreach ($list_split as $key => $word) 
+			{
+				$q1 = "SELECT * FROM `words` WHERE `word`='$word'";
+
+				if($query_run = mysqli_query($connection,$q1))
+				{
+
+					if(mysqli_num_rows($query_run) == 1 )
+					{	
+						//Word already in list. Do nothing
+					}
+					else
+					{
+						$q2= "INSERT INTO `words` (`word`) VALUES ('$word')";
+						mysqli_query($connection,$q2);
+					}
+						
+				}
+			}
+		}
+		
+		//Checking 
+		if($check=="standard") //standard check WDYL 
+		{
+			$url="http://www.wdylike.appspot.com/?q=".$sentence;
+			$res = get_curl($url);
+			if($res=="false")
+				echo "Good to go!";
+			else
+				echo "Oops. Seems like you used a bad word";
+		}
+		else if($check =="rmselect") //just check for selected words
+		{
+			$list= $_POST['list'];
+			$list = preg_replace("/[^a-zA-Z 0-9]+/", " ", $list);
+			$list_split =explode(" ", $list);
+			
+			$sentence = preg_replace("/[^a-zA-Z 0-9]+/", " ", $sentence);
+			$sent_split = explode(" ", $sentence);
+			foreach($list_split as $key => $word)
+			{
+				if (in_array($word, $sent_split))
+				{
+				   echo "Oops. Seems like you used a bad word";
+				}
+				else
+				{
+				   echo "Good to go!";
+				}
+			}
+		}
+		else if($check =="createcustom") //read from database
+		{
+			$sentence = preg_replace("/[^a-zA-Z 0-9]+/", " ", $sentence);
+			$sent_split = explode(" ", $sentence);
+			$flag=0; //to keep track if a bad word occured
+			foreach($sent_split as $key => $word)
+			{
+				$q3 = "SELECT * FROM `words` WHERE `word`='$word'";
+
+				if($query_run = mysqli_query($connection,$q3))
+				{
+
+					if(mysqli_num_rows($query_run) == 1 )
+					{	
+						echo "Oops. Seems like you used a bad word";
+						$flag=1;
+						break;
+					}
+					else
+					{
+						//Do nothing	
+					}		
+				}
+			}
+			if ($flag==0)
+				echo "Good to go!";
+		}			
+	}
+?>
+<div class="container">
+	<table width="800" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#cccccc">
+	    <tr>
+	        <form method="POST">
+	            <td>
+	                <table width="100%" border="0"  cellspacing="1" bgcolor="#ffffff">
+	                    <tr>
+	                        <td colspan="3"><strong><center>Enter text input to check</cemter></strong></td>
+	                    </tr>
+	            		<tr>
+	                    	<td width="78">Text input</td>
+	                        <td width="6">:</td>
+	                        <td width="500" ><textarea name="sentence" cols="50" rows="10"></textarea></td>
+	                    </tr>
+	                    <tr></tr>
+	                    <tr>
+	                       	<td colspan="6"><label><input type="checkbox" name="checktype" value="standard">Standard check</label><br></td>
+	                    </tr>
+ 	                    <tr>
+	                        <td colspan="6"><label><input type="checkbox" name="checktype" value="createcustom">Create Custom check</label><br></td>
+	                    </tr>
+	                    <tr>
+	                        <td colspan="6"><label><input type="checkbox" name="checktype" value="rmselect">Selective removal</label><br></td>
+	                    </tr>
+	                     <tr>
+	                        <td width="78">Custom list</td>
+	                        <td width="6">:</td>
+	                        <td colspan="6"><textarea name="list" cols="50" rows="2"></textarea></td>
+	                    </tr>			
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td><input type="submit" name="Submit" value="GO"></td>
+                         </tr>
+                    </table>            
+                </td>
+            </form>
+        </tr>
+    </table>
+</div>
+<hr>
+
+<?php include 'inc/layout/footer.inc.php';?>
+	
+
 
 	
