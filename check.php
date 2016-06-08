0bdead90d3e255138492f80e7a412f884f0b99be
Prachi <prachi121096@gmail.com>
2016-06-08 21:22:08 +0530

    Custom check

--- check.php
+++ check.php
@@ -9,7 +9,7 @@
 	{
 		$sentence= $_POST['sentence'];
 		$check = $_POST['checktype'];
-		//echo $check;
+		echo count($check);
 		if($check=="standard")
 		{
 			$url="http://www.wdylike.appspot.com/?q=".$sentence;
@@ -19,19 +19,21 @@
 			else
 				echo "Oops. Seems like you used a bad word";
 		}
-		else
-		$key  = 'ho';
-
-		$sentence = preg_replace("/[^a-zA-Z 0-9]+/", " ", $sentence);
-		$sent_split = explode(" ", $sentence);
-
-		if (in_array($key, $sent_split))
+		else if($check =="custom")
 		{
-		    echo "Word found";
-		}
-		else
-		{
-		    echo "Word not found";
-		}
+			$key  = 'ho';
+
+			$sentence = preg_replace("/[^a-zA-Z 0-9]+/", " ", $sentence);
+			$sent_split = explode(" ", $sentence);
+
+			if (in_array($key, $sent_split))
+			{
+			    echo "Word found";
+			}
+			else
+			{
+			    echo "Word not found";
+			}
+		}		
 	}
 ?>
